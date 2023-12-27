864c63513b7d7bed8b47add5b4b2fb0d87b0b3d0
Camila Rocio Diaz <camilarociodiaz@gmail.com>
2023-12-27 20:58:44 -0300

    prettier

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,24 +3,17 @@ import "./ItemDetail.css";
 import React from "react";
 
 export const ItemDetail = ({ productoProp }) => {
-
      return (
           <>
-               <div >
-                    <img alt="img" src={productoProp.img} style={{ width: 300 }} className="card-img-top" />
-
-                    < div  >
-
+               <div>
+                    <img alt="img" src={productoProp.img} style={{ width: 300, heigh: 300 }} className="card-img-top" />
+                    <div>
                          <h2> {productoProp.title}</h2>
                          <h6>{productoProp.detail}</h6>
                          <h4>${productoProp.price}</h4>
-
                     </div>
-
                </div>
           </>
-
-
      )
 }
 
