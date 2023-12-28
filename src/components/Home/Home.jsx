048239b758c1334f313a76498dfd587a4a11ee6c
Camila Rocio Diaz <camilarociodiaz@gmail.com>
2023-12-27 21:16:30 -0300

    change button for home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,21 +4,12 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 export const Home = () => {
-
     return (
-
         <>
-            <div >
-
-<Link to={"/category"} >  <button className="homeButton"> Ver productos </button> </Link>
-           
-                <img src="https://i.ibb.co/BtWtYPR/banner1.jpg" className="d-block w-100 banner" alt="..." />
-
+            <div>
+                <Link to={"/category"} >  <button className="homeButton"> Ver productos </button> </Link>
+                <img src="https://i.ibb.co/BtWtYPR/banner1.jpg" className="d-block w-100 banner" alt="banner" />
             </div>
-
-
         </>
-
     )
-
 }
